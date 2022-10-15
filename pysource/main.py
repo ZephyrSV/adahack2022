1cbccb9633f44f2d40589bb2ed9c74cd8b369ee5
james <james.thomson019@gmail.com>
2022-10-15 14:09:39 +0100

    workin

--- pysource/main.py
+++ pysource/main.py
@@ -1,5 +1,12 @@
 import tweepy
 from textblob import TextBlob
+import re, string
+import nltk
+from nltk import pos_tag
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+stop_words = stopwords.words('english')
 
 accessToken = open("../AccessToken.txt").read()
 accessTokenSecret = open("../AccessTokenSecret.txt").read()
@@ -12,17 +19,47 @@ auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
 client = tweepy.Client(bearer_token)
 
-search_term = "boris johnson"
+search_term = "police"
 search_term_set = set(search_term.split())
-print(search_term_set)
 search_query = search_term + " lang:en"
-tweets = client.search_recent_tweets(query=search_query, max_results=100)     #returns Response Object
+tweets = client.search_recent_tweets(query=search_query, max_results=100)  # returns Response Object
+
+
+def remove_noise(tweet_tokens, stop_words=()):
+    """
+    takes a list of strings
+    """
+
+    cleaned_tokens = []
+
+    for token, tag in pos_tag(tweet_tokens):
+        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|' \
+                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', token)
+        token = re.sub("(@[A-Za-z0-9_]+)", "", token)
+        token = re.sub("([$-_.…“,”!?]+)", "", token)
+
+        if tag.startswith("NN"):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+
+        lemmatizer = WordNetLemmatizer()
+        token = lemmatizer.lemmatize(token, pos)
+
+        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words and token != 'rt':
+            cleaned_tokens.append(token.lower())
+    return cleaned_tokens
+
 
 wordcount = {}
-word_sentiment= {}
+word_sentiment = {}
 for tweet in tweets[0]:
     text = tweet.text
-    words = set(text.lower().split())
+    words1 = text.lower().split()
+    words = set(remove_noise(words1, stop_words))
+    #print(words)
 
     if not words.intersection(search_term_set):
         continue
@@ -32,18 +69,20 @@ for tweet in tweets[0]:
         wordcount[word] += 1
         word_sentiment.setdefault(word, 0)
         word_sentiment[word] += sentiment
-    print(text)
-    print("The polarity is: " + str(TextBlob(text).sentiment.polarity) + ", The subjectivity is: " + str(
-            TextBlob(text).sentiment.subjectivity) + "\n")
+    #print("The polarity is: " + str(TextBlob(text).sentiment.polarity) + ", The subjectivity is: " + str(
+    #    TextBlob(text).sentiment.subjectivity) + "\n")
 
 wordcount_ = dict(sorted(wordcount.items(), key=lambda item: item[1], reverse=True))
 print(wordcount_)
 
 for word in word_sentiment:
-    word_sentiment[word] = word_sentiment[word]/wordcount[word]
+    word_sentiment[word] = word_sentiment[word] / wordcount[word]
 
     word_sentiment_ = [(key, word_sentiment[key]) for key in wordcount_.keys()]
 
 print(word_sentiment_)
 
+full_list = [[word, wordcount_[word], word_sentiment[word]] for word in wordcount_.keys()]
+
+print(full_list[:50])
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
