ae4d1dcbd31a1f9d35805bf636e25e46c2b8d6ac
pablito <114697865+B214895@users.noreply.github.com>
2022-10-15 12:44:11 +0100

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -34,6 +34,8 @@ def remove_noise(tweet_tokens, stop_words = ()):
         token = lemmatizer.lemmatize(token, pos)
 
         if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
+            if token[-1] == '.' or ',':
+                token = token[:-1]
             cleaned_tokens.append(token.lower())
     return cleaned_tokens
 
